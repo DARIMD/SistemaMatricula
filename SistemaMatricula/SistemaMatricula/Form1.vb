1c6a7a4561ccbb1dcfed189fa9b789bf47c77554
DARIMD <katherinedarielamendozadiaz@inued.net>
2024-05-28 08:35:49 -0600

    Add files via upload

---
+++ SistemaMatricula/SistemaMatricula/Form1.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Data.OleDb
+Public Class Form1
+    Private Sub btnConectar_Click(sender As Object, e As EventArgs) Handles btnConectar.Click
+        Dim conexion As New OleDbConnection("Provider=Microsoft.jet.oledb.4.0;data source=F:\Users\LAB4\Downloads\SISTEMA MATRICULA\BASE DE DATOS\MATRUCULA(2002-2003).mdb")
+        Try
+            conexion.Open()
+            MsgBox("conectado")
+
+            Dim query = "select * from ESTUDIANTE"
+            Dim adap As New OleDbDataAdapter(query, conexion)
+            Dim dt As New DataTable
+            adap.Fill(dt)
+            DataGridView1.DataSource = dt
+
+        Catch ex As Exception
+            MsgBox("No se conecto por:" & ex.Message)
+        End Try
+    End Sub
+End Class
